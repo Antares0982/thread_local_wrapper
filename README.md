5575178c2726910c7cf8b85168a7b9616abdbf2d
Antares <32589030+Antares0982@users.noreply.github.com>
2023-09-16 21:08:16 +0800

    :tada: tls wrapper

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# thread_local_wrapper
-Toy thread_local wrapper of workaround for broken MinGW-W64 thread model
+# Thread Local Wrapper
+Toy `thread_local` wrapper to implement a workaround for broken MinGW-W64 thread model. See more details [here](https://github.com/msys2/MINGW-packages/issues/2519).
+
+### How To Use
+
+```c++
+auto &object = Antares::tls_get<T>();
+```
+
---
+++ src/test_main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Antares on 2023/9/16.
+//
+
+#include "tls_wrapper.h"
+#include <memory>
+#include <thread>
+
+void test_body() {
+    auto &ptr = Antares::tls_get<std::shared_ptr<int>>();
+    ptr = std::make_shared<int>(1);
+}
+
+void test_mingw_fail() {
+    thread_local std::shared_ptr<int> ptr = std::make_shared<int>(0);
+    ptr = std::make_shared<int>(1);
+}
+
+int main() {
+    std::thread thread_test(test_body);
+    thread_test.join();
+    std::thread thread_mingw_fail(test_mingw_fail);
+    thread_mingw_fail.join();
+    return 0;
+}
